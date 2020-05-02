bb6ad27b6e21cc0250baf91d09681834e12ed4aa
iphone8xsmax <18183124885@163.com>
2020-05-02 23:07:20 +0800

    实现最简单的文件增删改查功能

---
+++ handler/handler.go
@@ -0,0 +1,149 @@
+package handler
+
+import (
+	"encoding/json"
+	"fileSystem/meta"
+	"fileSystem/util"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET"{
+		//返回上传html页面
+		data, err := ioutil.ReadFile("./static/view/index.html")
+		if err != nil{
+			io.WriteString(w, "internel server error")
+			return
+		}
+		io.WriteString(w, string(data))
+	}else if r.Method == "POST"{
+		//接收文件流及存储到本地
+		file, head, err := r.FormFile("file")
+		if err != nil{
+			fmt.Printf("Failed to get data, err %s", err.Error())
+			return
+		}
+		defer file.Close()
+
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "./temp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+		
+		//创建本地文件接收
+		newFile, err := os.Create(fileMeta.Location)
+		if err != nil{
+			fmt.Printf("Failed to create file, err: %s", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		//copy文件内容到新文件中
+		fileMeta.FileSize, err = io.Copy(newFile, file)
+		if err != nil{
+			fmt.Printf("Failed to save data into file, err: %s", err.Error())
+			return
+		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+		// 游标重新回到文件头部
+		newFile.Seek(0, 0)
+
+		fmt.Println(fileMeta)
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
+	}
+}
+
+//提示上传完成
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload Successly!")
+}
+
+//获取文件元信息
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form["filehash"][0]
+	fmt.Println(filehash)
+	fMeta := meta.GetFileMeta(filehash)
+
+	data, err := json.Marshal(fMeta)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//下载文件
+func DownloadHandler(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+
+	f, err := os.Open(fm.Location)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	//小文件
+	data, err := ioutil.ReadAll(f)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("content-disposition", "attachment;filename=\"" + fm.FileName + "\"")
+	w.Write(data)
+}
+
+//更新元信息（重命名）
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	opType := r.Form.Get("op")
+	fileSha1 := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0"{
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	if r.Method != "POST"{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	curFileMeta := meta.GetFileMeta(fileSha1)
+	curFileMeta.FileName = newFileName
+	meta.UpdateFileMeta(curFileMeta)
+
+	data, err := json.Marshal(curFileMeta)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+//删除文件及元信息
+func FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fileSha1 := r.Form.Get("filehash")
+
+	fMeta := meta.GetFileMeta(fileSha1)
+	os.Remove(fMeta.Location)
+
+	meta.RemoveFileMeta(fileSha1)
+	w.WriteHeader(http.StatusOK)
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fileSystem/handler"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
+	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
+	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil{
+		fmt.Printf("failed to start server, err:%s", err.Error())
+	}
+}
